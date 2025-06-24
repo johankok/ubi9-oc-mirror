8a8bdb989d0a55f6350c375954cbe502a8da8675
Johan Kok <johankok@users.noreply.github.com>
2025-06-24 12:02:40 +0200

    Update to 4.19.0

--- download-oc-mirror.sh
+++ download-oc-mirror.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Download oc client and oc-mirror
-VERSION="4.18.9"
+VERSION="4.19.0"
 BASE="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/$VERSION"
 
 for file in oc-mirror.tar.gz openshift-client-linux-$VERSION.tar.gz sha256sum.txt
